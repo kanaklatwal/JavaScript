b58ab7d06b79cb9180e650a146dd088dd72a43ed
Kanak Latwal <kanaklatwal@gmail.com>
2026-01-19 06:35:55 +0000

    datatypes conversion in javascript

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -20,3 +20,39 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);//33
 console.log(typeof stringNumber)// string
+
+
+// ****************Operations****************
+let value = 3
+let negValue = -value
+console.log(negValue);//-3
+
+/*
+console.log(2+2)//4
+console.log(2-2)//0
+console.log(2*2)//4
+console.log(2**2)//4
+console.log(2/3)//0.6666666666666
+console.log(2%3)//2
+*/
+
+let str1 = "hello"
+let str2 = " Hitesh"
+let str3 = str1+str2
+console.log(str3);
+
+console.log("1"+2)//12
+console.log(1+"2")//12
+console.log("1"+2+2)//122
+console.log(1+2+"2")//32
+console.log("1"+"2"+"2")//122
+
+console.log(+true)//1
+console.log(+"")//0
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)//101
