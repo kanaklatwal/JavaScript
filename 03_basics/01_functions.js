762a28d02392629c16c26e4c76a9b46adda9caf6
Kanak Latwal <kanaklatwal@gmail.com>
2026-01-23 07:00:25 +0000

    Functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,42 @@
+function sayName(){
+    console.log("H");
+    console.log("A");
+    console.log("T")
+    console.log("T");
+    console.log("R");
+    console.log("I");
+}
+
+sayName()
+// H
+// A
+// T
+// T
+// R
+// I
+
+function addTwoNumbers(number1, number2){
+    console.log(number1 + number2);
+}
+addTwoNumbers(3,4)//7
+addTwoNumbers(3,"4")//34
+addTwoNumbers(3,null)//3
+const result = addTwoNumbers(3,5)//8
+console.log("Result: ", result);//Result:  undefined
+
+function add2number(number1, number2){
+    let result = number1 + number2
+    return result;
+}
+const r = add2number(7,8)
+console.log("Result: ", r);//Result:  15
+
+function loginUserMessage(username){
+    if(!username){
+        console.log("Please enter a username");
+    }
+      return `${username} just logged in`
+}
+
+console.log(loginUserMessage("hitesh"))//hitesh just logged in
+console.log(loginUserMessage())//undefined just logged in
