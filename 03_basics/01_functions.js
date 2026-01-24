bd96e376d5aad52a93d6dc89e5eaca0090e613e6
Kanak Latwal <kanaklatwal@gmail.com>
2026-01-24 12:26:46 +0000

    Functions in javascript

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -40,3 +40,29 @@ function loginUserMessage(username){
 
 console.log(loginUserMessage("hitesh"))//hitesh just logged in
 console.log(loginUserMessage())//undefined just logged in
+
+function calculateCartPrice(...num1){
+     return num1
+}
+console.log(calculateCartPrice(200, 400, 500, 2000))//[ 200, 400, 500, 2000 ]
+
+const user = {
+    username: "hitesh",
+    price: 199
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+handleObject(user)//Username is hitesh and price is 199
+handleObject({
+    username: "sam",
+    price: 399
+})//Username is sam and price is 399
+
+const myNewArray = [200, 400, 100, 600];
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+console.log(returnSecondValue(myNewArray));//400
+console.log(returnSecondValue([200, 400, 500, 1000]))//400
