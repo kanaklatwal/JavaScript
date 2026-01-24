41b90e5c6d06897f40d154905bf26cda180acbe8
Kanak Latwal <kanaklatwal@gmail.com>
2026-01-24 15:05:28 +0000

    Scope in javascript

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -1,6 +1,6 @@
 var c = 300
 let a =400
-if(true){
+if(true){   // {}are scope
     let a= 10
     const b =20
     var c =30
@@ -8,4 +8,37 @@ if(true){
 }
 console.log(a)//400
 console.log(c)//30
-console.log(b)//error
+//console.log(b)//error
+
+function one(){
+    const username = "kalpjeet"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    //console.log(website);// Error
+    two()
+}
+if(true){
+    const username = "hitesh"
+    if(username == "hitesh"){
+        const website = " youtube"
+        console.log(username  + website); //hitesh youtube
+    }
+   // console.log(website); error
+}
+//console.log(username);  error
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+function addone(num){
+    return num+1
+}
+console.log(addone(5))//6
+
+const addTwo = function(num){
+    return num+2
+}
+console.log(addTwo(5)) //7
