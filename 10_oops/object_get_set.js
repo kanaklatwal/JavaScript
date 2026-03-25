1f34de0dccbb9d2a47c1b5bc18703e356db30ade
Kanak Latwal <kanaklatwal@gmail.com>
2026-03-25 11:10:53 +0000

    Object Based getter and setter

---
+++ 10_oops/object_get_set.js
@@ -0,0 +1,12 @@
+const User ={
+    _email: 'h@knnhhh.com',
+    _password: "abc",
+    get email(){
+        return this._email.toUpperCase()
+    },
+    set email(value){
+        this._email = value
+    }
+}
+const tea = Object.create(User)
+console.log(tea.email)
